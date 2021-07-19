0b105e828c0449575cc53be8ca549a6a5ef43243
Shrivastav <aditya.shrivastav@SW-LP02523.domain.name>
2021-07-19 23:27:14 +0530

    Lazy loading for images added. Minor UX changes.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,36 +1,38 @@
 import './card-styles.css';
 import { Link } from 'react-router-dom';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const Card = (props) => {
 	const { country } = props;
 
 	return (
 		<div className="Card" id={country.numericCode}>
-			<div className="card-image">
-				<img src={country.flag} alt="" />
-			</div>
-			<div className="card-body">
-				<div>
-					<Link
-						to={`/details/${country.alpha3Code}`}
-						className="card-heading"
-					>
-						<h3>{country.name}</h3>
-					</Link>
+			<Link
+				to={`/details/${country.alpha3Code}`}
+				className="card-link"
+			>
+				<div className="card-image">
+					<LazyLoadImage effect="blur" src={country.flag} alt={`${country.name}`+ " flag"} />
 				</div>
-				<div className="card-text">
-					<p>
-						<span>Population:</span> {country.population}
-					</p>
-					<p>
-						<span>Region:</span> {country.region}
-					</p>
-					<p>
-						<span>Capital:</span>{' '}
-						{country.capital ? country.capital : 'N/A'}
-					</p>
+				<div className="card-body">
+					<div>
+						<h3 className="card-heading">{country.name}</h3>
+					</div>
+					<div className="card-text">
+						<p>
+							<span>Population:</span> {country.population}
+						</p>
+						<p>
+							<span>Region:</span> {country.region}
+						</p>
+						<p>
+							<span>Capital:</span>{' '}
+							{country.capital ? country.capital : 'N/A'}
+						</p>
+					</div>
 				</div>
-			</div>
+			</Link>
 		</div>
 	);
 };
