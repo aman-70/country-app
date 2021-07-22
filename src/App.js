7a9ede06be968b5890c2b09e0322c61491aa1499
Aditya Shrivastav <shriadishri@gmail.com>
2021-07-22 13:41:33 +0530

    Changing themes using localStorage.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,64 @@
 import './css/style.css';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Dashboard from './components/Dashboard/Dashboard';
 import CountryDetails from './components/CountryDetails/CountryDetails';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
-	const [theme, setTheme] = useState('lightTheme');
-	const [darkOn, setDarkOn] = useState(false);
+	// const [theme, setTheme] = useState(null);
+	// const [darkOn, setDarkOn] = useState(false);
+	
+	// const clickHandler = () => {
+	// 	if(darkOn === false) {
+	// 		setDarkOn(true);
+	// 	} else if(darkOn === true) {
+	// 		setDarkOn(false);
+	// 	}
+	// }
 
-	const clickHandler = () => {
-		if(darkOn === false) {
-			setDarkOn(true);
-		} else if(darkOn === true) {
-			setDarkOn(false);
-		}
+	// useEffect(() => {
+	// 	if(darkOn === false) {
+	// 		setTheme('lightTheme');
+	// 	} else if(darkOn === true) {
+	// 		setTheme('darkTheme');
+	// 	}
+	// }, [darkOn]);
+
+	let clickedClass = "clicked";
+	const body = document.body;
+	const lightTheme = "light";
+	const darkTheme = "dark";
+	let theme;
+
+	if (localStorage) {
+		theme = localStorage.getItem("theme");
+	}
+	
+	if (theme === lightTheme || theme === darkTheme) {
+		body.classList.add(theme);
+	} else {
+		body.classList.add(lightTheme);
 	}
 
-	useEffect(() => {
-		if(darkOn === false) {
-			setTheme('lightTheme');
-		} else if(darkOn === true) {
-			setTheme('darkTheme');
+	const clickHandler = (e) => {
+		if (theme === darkTheme) {
+		  body.classList.replace(darkTheme, lightTheme);
+		  e.target.classList.remove(clickedClass);
+		  localStorage.setItem("theme", "light");
+		  theme = lightTheme;
+		} else {
+		  body.classList.replace(lightTheme, darkTheme);
+		  e.target.classList.add(clickedClass);
+		  localStorage.setItem("theme", "dark");
+		  theme = darkTheme;
 		}
-	}, [darkOn]);
+	  }
 
 	return (
 		<Router>
-			<main className={`App ${theme}`}>
+			{/* <main id="App" className={`App ${theme}`}> */}
+			<main id="App" className={`App`}>
 				<div className="container">
 					<Navbar clickHandler={clickHandler} theme={theme} />
 					<Switch>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,17 @@
 import { Link } from 'react-router-dom';
 
-const Navbar = ({ clickHandler, theme }) => {
+const Navbar = ({ clickHandler }) => {
+
 	return (
 		<header className="Navbar">
 			<div className="navbar-wrapper">
 				<Link to="/" className="navbar-heading"><h2>Where in the world?</h2></Link>
-				<div className="dark-mode" onClick={clickHandler}>
+				<div className="dark-mode" onClick={e => clickHandler(e)}>
 					<span>
 						<i className="fas fa-moon fa-md"></i>
 						<i className="fas fa-sun fa-lg"></i>
 					</span>
-					<p>{theme === "darkTheme" ? "Light" : "Dark"}</p>
+					<p>Dark</p>
 				</div>
 			</div>
 		</header>
